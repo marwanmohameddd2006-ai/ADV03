9188c1266eff432a60be95a430611e94c0d5851e
marwanmohameddd2006-ai <marwanmohameddd2006@gmail.com>
2026-08-30 08:54:38 +0300

    Exercise 1: Student Grade Manager

--- Assignment_3_ADV/Program.cs
+++ Assignment_3_ADV/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Exercise 1: Student Grade Manager
+            //1.
+            //List<int> grades = [85, 92, 78, 95, 88, 70, 100, 65];
+
+            //2.
+            //Console.WriteLine($"Count = {grades.Count} , First grad = {grades[0]}/100 , Last grad  = {grades[^1]}/100");
+
+            //3.
+            //grades.Sort();
+            //foreach( int grade in grades )
+            //{
+            //    Console.WriteLine(grade);
+            //}
+
+            //4.
+            //int first_grade_above90 = grades.Find(x=>x>90);
+            //Console.WriteLine($"first grade above 90  = {first_grade_above90}");
+
+            //5.
+            //List<int> failinggrades = grades.FindAll(x => x < 75);
+            //foreach (int grade in failinggrades)
+            //{
+            //    Console.WriteLine(grade);
+            //}
+
+            //6.
+            //failinggrades.Clear();
+
+            //7.
+            //Console.WriteLine(grades.Contains(100)) ;
+
+            //8.
+            //Failed
+
+            #endregion
         }
     }
 }
